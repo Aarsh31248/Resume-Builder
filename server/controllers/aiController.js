46faa23f1d2945c46e41c0570c33034d15d257c0
Aarsh Singh <starfire9599@gmail.com>
2026-04-07 23:57:39 +0530

    feat: API for uploading a resume to the database

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,3 +1,5 @@
+import Resume from "../models/Resume.js";
+
 // Controller for enhancing a resume professional summary
 // POST: "/api/ai/enhance-pro-sum"
 export const enhanceProfessionalSummary = async (req, res) => {
@@ -5,7 +7,7 @@ export const enhanceProfessionalSummary = async (req, res) => {
     const { userContent } = req.body;
 
     if (!userContent) {
-      return res.status(400).json({ message: "Missing requires fields" });
+      return res.status(400).json({ message: "Missing required fields" });
     }
 
     const response = await ai.chat.completions.create({
@@ -23,7 +25,7 @@ export const enhanceProfessionalSummary = async (req, res) => {
       ],
     });
 
-    const enhancedContent = response.choices[0].message;
+    const enhancedContent = response.choices[0].message.content;
 
     return res.status(200).json({ enhancedContent });
   } catch (error) {
@@ -38,7 +40,7 @@ export const enhanceJobDescription = async (req, res) => {
     const { userContent } = req.body;
 
     if (!userContent) {
-      return res.status(400).json({ message: "Missing requires fields" });
+      return res.status(400).json({ message: "Missing required fields" });
     }
 
     const response = await ai.chat.completions.create({
@@ -56,9 +58,95 @@ export const enhanceJobDescription = async (req, res) => {
       ],
     });
 
-    const enhancedContent = response.choices[0].message;
+    const enhancedContent = response.choices[0].message.content;
 
     return res.status(200).json({ enhancedContent });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// Controller for uploading a resume to the database
+// POST: "/api/ai/upload-resume"
+export const uploadResume = async (req, res) => {
+  try {
+    const { resumeText, title } = req.body;
+    const userId = req.userId;
+
+    if (!resumeText) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const systemPrompt =
+      "You are an expert AI agest to extract data from resume.";
+
+    const userPrompt = `extract data from this resume: ${resumeText}
+    
+    Provide data in the following JSON format with no additional text before or after:
+
+    {
+    professional_summary: { type: String, default: "" },
+    skills: [{ type: String }],
+    personal_info: {
+      image: { type: String, default: "" },
+      full_name: { type: String, default: "" },
+      profession: { type: String, default: "" },
+      email: { type: String, default: "" },
+      phone: { type: String, default: "" },
+      location: { type: String, default: "" },
+      linkedin: { type: String, default: "" },
+      website: { type: String, default: "" },
+    },
+    experience: [
+      {
+        company: { type: String },
+        position: { type: String },
+        start_date: { type: String },
+        end_date: { type: String },
+        description: { type: String },
+        is_current: { type: Boolean },
+      },
+    ],
+    projects: [
+      {
+        name: { type: String },
+        type: { type: String },
+        description: { type: String },
+      },
+    ],
+    education: [
+      {
+        institution: { type: String },
+        degree: { type: String },
+        field: { type: String },
+        graduation_date: { type: String },
+        gpa: { type: String },
+      },
+    ],
+    }
+    `;
+
+    const response = await ai.chat.completions.create({
+      model: process.env.OPENAI_MODEL,
+      messages: [
+        {
+          role: "system",
+          content: systemPrompt,
+        },
+        {
+          role: "user",
+          content: userPrompt,
+        },
+      ],
+      response_format: { type: "json_object" },
+    });
+
+    const extractedData = response.choices[0].message.content;
+    const parsedData = JSON.parse(extractedData);
+
+    const newResume = await Resume.create({ userId, title, ...parsedData });
+
+    res.json({ resumeId: newResume._id });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
